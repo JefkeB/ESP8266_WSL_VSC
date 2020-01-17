0714f9b230f86b416663c71633dbacf76f04b69e
JefkeB <37012100+JefkeB@users.noreply.github.com>
2020-01-17 17:49:43 +0100

    Added ESP8266_Dev files
    The files needed to setup the C:\ESP8266_Dev location

---
+++ ESP8266_Dev/start_environment.bat
@@ -0,0 +1,3 @@
+set wsl_project_folder_root=/mnt/d/ESP8266_Projects
+set wsl_project_folder_drive=/mnt/d
+start "" "%CD%\VSC\Code.exe" --extensionHomePath=%CD%\VSC_port\Extensions --user-data-dir=%CD%\VSC_port\data 	
