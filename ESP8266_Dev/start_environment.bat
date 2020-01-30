f847541eed1683a6f5b91910f9cdc62f0d6b4713
JefkeB <github@bergenv.nl>
2020-01-30 11:07:03 +0100

    added more parameters to start_environment.bat
    This is done for now in this way. Need to put this in an extension for easier access.

--- ESP8266_Dev/start_environment.bat
+++ ESP8266_Dev/start_environment.bat
@@ -1,3 +1,6 @@
+PATH=%CD%\Tools_Win;%PATH%
 set wsl_project_folder_root=/mnt/d/ESP8266_Projects
 set wsl_project_folder_drive=/mnt/d
+set wsl_espport=/dev/ttyS7
+set wsl_remoteupdate_ip=192.168.2.31
 start "" "%CD%\VSC\Code.exe" --extensionHomePath=%CD%\VSC_port\Extensions --user-data-dir=%CD%\VSC_port\data 	
